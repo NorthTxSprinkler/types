aaeaf60fb5b1b9aff161dc5365c99ee07e08b595
Kevin Towe <kevintowe@Kevins-iMac.attlocal.net>
2020-10-18 11:45:57 -0500

    save

---
+++ src/index.ts
@@ -0,0 +1,72 @@
+export interface Project {
+  id?: string;
+  projectName?: string;
+  description?: string;
+  startDate?: any;
+  finishDate?: any;
+  relatedEstimates?: Array<string>;
+  estimatedCost?: number;
+  actualCost?: number;
+  estimatedCostItems?: Array<{ cost: number; description: string }>;
+  actualCostItems?: Array<{ cost: number; description: string }>;
+}
+
+export interface Client {
+  id?: string;
+}
+
+export interface User {
+  id: string;
+  name: string;
+  admin: boolean;
+  instantiatePhoneSystem: boolean;
+}
+
+export interface IrrigationApt {
+  customer: {
+    name: string;
+    tel?: string;
+    email?: string;
+    address?: any;
+  };
+  time: any;
+  notes: Array<string>;
+}
+
+/**
+ * Images Pipeline
+ */
+export interface ImageForPipeline {
+  id?: string;
+  imageUrl: string;
+  from: string;
+  processed: boolean;
+  processedAt?: any;
+  submittedAt: any;
+}
+
+/**
+ * Entity Types
+ */
+export interface Customer {
+  id?: string;
+  intuitId?: string;
+  name?: string;
+  address?: Address;
+  email?: string;
+  phone?: string;
+  active?: boolean;
+  balance?: number;
+  createdTime?: any;
+}
+
+export interface Address {
+  city: string;
+  countrySubDivisionCode: string;
+  id: string;
+  lat: string;
+  lng: string;
+  line1: string;
+  line2: string;
+  postalCode: string;
+}
